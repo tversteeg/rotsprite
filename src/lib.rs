0ff59a34dbec9714cc47f52ce2c87e9027fad3d4
Thomas Versteeg <thomasversteeg@gmx.com>
2020-02-07 21:24:16 +0100

    Start implementing rotation algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -4,57 +4,106 @@
 
 use thiserror::Error;
 
+use std::f64;
+
 #[derive(Error, Debug, PartialEq)]
 pub enum Error {
     #[error("image size doesn't match with supplied width")]
     ImageSizeMismatch,
 }
 
-// Convert a single pixel to an upscaled 2x2 block
-#[inline(always)]
-fn calculate_scale2x_block<P>(center: &P, up: &P, left: &P, down: &P, right: &P) -> (P, P, P, P)
+/// Rotate a sprite based on any pixel format implementing the `Eq` and `Clone` traits.
+///
+/// Rotation is in degrees (0-360).
+/// The size of the resulting vector will be bigger if the rotation isn't exactly 0.0, 90.0, 180.0 or 270.0 degrees.
+/// The width and the height will be swapped at angles of 90.0 and 270.0.
+pub fn rotsprite<P>(
+    buf: &[P],
+    empty_color: &P,
+    width: usize,
+    rotation: f64,
+) -> Result<(usize, usize, Vec<P>), Error>
 where
     P: Eq + Clone,
 {
-    (
-        (if down == up && down != right && up != left {
-            up
-        } else {
-            center
-        })
-        .clone(),
-        (if up == left && up != down && left != right {
-            left
-        } else {
-            center
-        })
-        .clone(),
-        (if right == down && right != left && down != up {
-            down
-        } else {
-            center
-        })
-        .clone(),
-        (if left == right && left != up && right != down {
-            right
-        } else {
-            center
-        })
-        .clone(),
-    )
+    // If there's no rotation we don't have to do anything
+    if rotation == 0.0 {
+        return Ok((width, buf.len() / width, buf.to_vec()));
+    }
+
+    let len = buf.len();
+    if len % width != 0 {
+        return Err(Error::ImageSizeMismatch);
+    }
+    let height = len / width;
+
+    // Upscale the image using the scale2x algorithm
+    // 2x
+    let scaled = scale2x(buf, width, height);
+    // 4x
+    let scaled = scale2x(&scaled, width, height);
+    // 8x
+    let scaled = scale2x(&scaled, width, height);
+
+    // Rotate the image
+    let rotated = rotate(
+        &scaled,
+        empty_color,
+        width as f64,
+        height as f64,
+        rotation % 360.0,
+    );
+
+    Ok(rotated)
 }
 
-// Apply the block on the buffer
-#[inline(always)]
-fn apply_scale2x_block<P>(scaled: &mut [P], pos: usize, width: usize, pixels: (&P, &P, &P, &P, &P))
+// Algorithm for rotating the image
+fn rotate<P>(
+    buf: &[P],
+    empty_color: &P,
+    width: f64,
+    height: f64,
+    rotation: f64,
+) -> (usize, usize, Vec<P>)
 where
     P: Eq + Clone,
 {
-    let block_pixels = calculate_scale2x_block(pixels.0, pixels.1, pixels.2, pixels.3, pixels.4);
-    scaled[pos] = block_pixels.0;
-    scaled[pos + 1] = block_pixels.1;
-    scaled[pos + width] = block_pixels.2;
-    scaled[pos + width + 1] = block_pixels.3;
+    let radians = rotation * f64::consts::PI / 180.0;
+    let sin_angle = (radians).sin();
+    let cos_angle = (radians).cos();
+
+    // First calculate the new size
+    let half_width = width / 2.0;
+    let half_width_cos = half_width * cos_angle;
+    let half_width_sin = half_width * sin_angle;
+    let half_height = height / 2.0;
+    let half_height_sin = half_height * sin_angle;
+    let half_height_cos = half_height * cos_angle;
+    let x_coords = [
+        half_width_cos + half_height_sin,
+        -half_width_cos + half_height_sin,
+        -half_width_cos - half_height_sin,
+        half_width_cos - half_height_sin,
+    ];
+    let y_coords = [
+        half_width_sin + half_height_cos,
+        -half_width_sin + half_height_cos,
+        -half_width_sin - half_height_cos,
+        half_width_sin - half_height_cos,
+    ];
+
+    // Get the min and max values of all the coordinates
+    let min_x = x_coords.iter().cloned().fold(1.0f64 / 0.0f64, f64::min);
+    let max_x = x_coords.iter().cloned().fold(0.0f64 / 0.0f64, f64::max);
+    let min_y = y_coords.iter().cloned().fold(1.0f64 / 0.0f64, f64::min);
+    let max_y = y_coords.iter().cloned().fold(0.0f64 / 0.0f64, f64::max);
+
+    let new_width = (max_x - min_x).abs().ceil() as usize;
+    let new_height = (max_y - min_y).abs().ceil() as usize;
+
+    let mut rotated = vec![empty_color.clone(); new_width * new_height];
+
+    (new_width, new_height, rotated)
 }
 
 // Algorithm for fast upscaling of pixel art sprites
@@ -188,35 +237,51 @@ where
     scaled
 }
 
-/// Rotate a sprite based on any pixel format implementing the `Eq` and `Clone` traits.
-///
-/// Rotation is in degrees (0-360).
-/// The size of the resulting vector will be bigger if the rotation isn't exactly 0.0, 90.0, 180.0 or 270.0 degrees.
-pub fn rotsprite<P>(buf: &[P], width: usize, rotation: f64) -> Result<Vec<P>, Error>
+// Apply the block on the buffer
+#[inline(always)]
+fn apply_scale2x_block<P>(scaled: &mut [P], pos: usize, width: usize, pixels: (&P, &P, &P, &P, &P))
 where
     P: Eq + Clone,
 {
-    // If there's no rotation we don't have to do anything
-    if rotation == 0.0 {
-        return Ok(buf.to_vec());
-    }
+    let block_pixels = calculate_scale2x_block(pixels.0, pixels.1, pixels.2, pixels.3, pixels.4);
+    scaled[pos] = block_pixels.0;
+    scaled[pos + 1] = block_pixels.1;
+    scaled[pos + width] = block_pixels.2;
+    scaled[pos + width + 1] = block_pixels.3;
+}
 
-    let len = buf.len();
-    if len % width != 0 {
-        return Err(Error::ImageSizeMismatch);
-    }
-    let height = len / width;
-
-    // Upscale the image using the scale2x algorithm
-
-    // 2x
-    let scaled = scale2x(buf, width, height);
-    // 4x
-    let scaled = scale2x(&scaled, width, height);
-    // 8x
-    let scaled = scale2x(&scaled, width, height);
-
-    Ok(scaled)
+// Convert a single pixel to an upscaled 2x2 block
+#[inline(always)]
+fn calculate_scale2x_block<P>(center: &P, up: &P, left: &P, down: &P, right: &P) -> (P, P, P, P)
+where
+    P: Eq + Clone,
+{
+    (
+        (if down == up && down != right && up != left {
+            up
+        } else {
+            center
+        })
+        .clone(),
+        (if up == left && up != down && left != right {
+            left
+        } else {
+            center
+        })
+        .clone(),
+        (if right == down && right != left && down != up {
+            down
+        } else {
+            center
+        })
+        .clone(),
+        (if left == right && left != up && right != down {
+            right
+        } else {
+            center
+        })
+        .clone(),
+    )
 }
 
 #[cfg(test)]
@@ -224,9 +289,57 @@ mod tests {
     use super::*;
 
     #[test]
+    fn rotation_equality() -> Result<(), Error> {
+        let buf = [1, 2, 3, 4, 5, 6].to_vec();
+        assert_eq!(
+            rotsprite(&buf, &0, 3, 45.0)?,
+            rotsprite(&buf, &0, 3, 45.0 + 360.0)?
+        );
+        assert_eq!(
+            rotsprite(&buf, &0, 3, 45.0)?,
+            rotsprite(&buf, &0, 3, 45.0 - 360.0)?
+        );
+        assert_eq!(
+            rotsprite(&buf, &0, 3, 12.0)?,
+            rotsprite(&buf, &0, 3, 12.0 - 360.0)?
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn rotation_size() -> Result<(), Error> {
+        let buf = [1, 2, 3, 4, 5, 6].to_vec();
+        let (w, h, _) = rotsprite(&buf, &0, 3, 45.0)?;
+        assert_eq!(w, 4);
+        assert_eq!(h, 4);
+        let (w, h, _) = rotsprite(&buf, &0, 3, 90.0)?;
+        assert_eq!(w, 2);
+        assert_eq!(h, 3);
+        let (w, h, _) = rotsprite(&buf, &0, 3, 180.0)?;
+        assert_eq!(w, 3);
+        assert_eq!(h, 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn rotation_test() -> Result<(), Error> {
+        let buf = [1, 2, 3, 4, 5, 6].to_vec();
+        let (w, h, new) = rotsprite(&buf, &0, 3, 90.0)?;
+        assert_eq!(w, 2);
+        assert_eq!(h, 3);
+        assert_eq!(new, [4, 1, 5, 2, 6, 3]);
+
+        Ok(())
+    }
+
+    #[test]
     fn no_rotation_test() -> Result<(), Error> {
         let buf = [1, 0, 0, 1, 1, 0].to_vec();
-        let new = rotsprite(&buf, 2, 0.0)?;
+        let (w, h, new) = rotsprite(&buf, &-1, 2, 0.0)?;
+        assert_eq!(w, 2);
+        assert_eq!(h, 3);
         assert_eq!(buf, new);
 
         Ok(())
@@ -235,7 +348,7 @@ mod tests {
     #[test]
     fn size_mismatch_error_test() {
         assert_eq!(
-            rotsprite(&[0, 0, 0, 0, 0], 2, 1.0).unwrap_err(),
+            rotsprite(&[0, 0, 0, 0, 0], &-1, 2, 1.0).unwrap_err(),
             Error::ImageSizeMismatch
         );
     }
@@ -256,5 +369,12 @@ mod tests {
         cmp.extend([7, 7, 8, 8, 9, 9].iter());
         cmp.extend([7, 7, 8, 8, 9, 9].iter());
         assert_eq!(new, cmp);
+
+        let buf = [1, 2, 3, 4, 5, 6];
+        let new = scale2x(&buf, 3, 2);
+        assert_eq!(
+            new,
+            [1, 1, 2, 2, 3, 3, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 4, 4, 5, 5, 6, 6]
+        );
     }
 }
