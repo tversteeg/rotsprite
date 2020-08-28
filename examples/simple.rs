ea43d0824758b89a04d05f901e574b9061134ff7
Brian McKeever <keever@gmail.com>
2020-08-28 16:40:21 -0600

    added simple example

---
+++ examples/simple.rs
@@ -0,0 +1,29 @@
+use image::{GenericImageView, Rgba, RgbaImage};
+use rotsprite::rotsprite;
+
+fn main() {
+    // Open the image
+    let img = image::open("examples/threeforms.png").unwrap();
+    let width = img.width() as usize;
+    let image: &RgbaImage = img
+        .as_rgba8()
+        .expect("Could not convert image to RGBA8 array");
+
+    let pixels: Vec<Rgba<u8>> = image.pixels().copied().collect();
+    let unfound_color = Rgba([255, 255, 255, 255]);
+    let rotation_angle: f64 = 45.0; //Rotate in increments of 15 degrees
+    let (rotated_width, rotated_height, rotated) = rotsprite(
+        &pixels,
+        &unfound_color, // The color for pixels that couldn't be found
+        width,
+        rotation_angle,
+    )
+    .expect("Could not rotate sprite");
+
+    let rotated_image = RgbaImage::from_fn(rotated_width as u32, rotated_height as u32, |x, y| {
+        rotated[rotated_width * y as usize + x as usize]
+    });
+    rotated_image
+        .save("rotated.png")
+        .expect("Failed to save the image");
+}
